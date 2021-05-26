dd17878d0390b91d686ff7de968ef54e2cf55032
Emil Andersson <e.andersson22@gmail.com>
2021-05-26 12:01:32 +0200

    dynamically update multiplication depending on amount

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,25 +1,20 @@
 import Axios from "axios";
 import React, { useEffect, useState, useRef } from "react";
-import { Typography, Container, Input, Button } from "@material-ui/core";
+import {
+  Typography,
+  Container,
+  Input,
+  Button,
+  TextField,
+} from "@material-ui/core";
 import ArrowRightAltIcon from "@material-ui/icons/ArrowRightAlt";
 
 export default function Converter() {
-  let exchangeValues = {};
-  const currencies = [
-    {
-      value: "USD",
-      label: "$",
-    },
-    {
-      value: "EUR",
-      label: "€",
-    },
-    {
-      value: "SEK",
-      label: "SEK",
-    },
-  ];
-  useEffect(() => {
+  const [exchangeValues, setExchangeValues] = useState([]);
+  const [amount, setAmount] = useState(1);
+  const [shownExchangeValue, setShownExchangeValue] = useState(1);
+
+  /* useEffect(() => {
     Axios.all([
       Axios.get(
         "https://free.currconv.com/api/v7/convert?q=EUR_SEK&compact=ultra&apiKey=dedc3fcbb37cec30f6ea"
@@ -33,18 +28,36 @@ export default function Converter() {
     ])
       .then(
         Axios.spread((responseEUR, responseUSD, responseGBP) => {
-          exchangeValues = {
+          setExchangeValues({
             EUR: responseEUR.data.EUR_SEK,
             USD: responseUSD.data.USD_SEK,
             GBP: responseGBP.data.GBP_SEK,
-          };
+          });
           console.log(exchangeValues);
         })
       )
       .catch((error) => {
         console.log(error);
       });
+  }, []); */
+
+  useEffect(() => {
+    setExchangeValues({
+      EUR: 11,
+      USD: 12,
+      GBP: 13,
+    });
   }, []);
+
+  useEffect(() => {
+    setShownExchangeValue(amount * exchangeValues.EUR);
+  }, [amount]);
+
+  useEffect(() => {
+    if (exchangeValues !== []) {
+      setShownExchangeValue(exchangeValues.EUR);
+    }
+  }, [exchangeValues]);
 
   return (
     <>
@@ -55,9 +68,12 @@ export default function Converter() {
           </Typography>
           <Container>
             <form>
-              <Input placeholder={exchangeValues.EUR} />
+              <TextField
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
               <ArrowRightAltIcon style={{ fontSize: 40 }} />
-              <Input placeholder="1" />
+              <TextField value={shownExchangeValue} />
             </form>
           </Container>
         </div>
